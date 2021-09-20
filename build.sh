d3e349af27c2d22b1f5ebec97a0904a1a308a77b
Killaship <69988679+Killaship@users.noreply.github.com>
2021-09-20 09:36:11 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 git pull
-nasm -f bin src/mbr.asm -o mbr
-nasm -f bin src/stage2.asm -o 
-cat mbr stage2 < pboot
-# simply do "cat pboot kernel > os" to get the os image to be booted
+nasm -f bin src/mbr.asm -o pboot
+#change for any extra files later on
 export DISPLAY=:0.0
 qemu-system-i386 pboot
